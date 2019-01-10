3f7b1d2340cf61d4c2ea246b212ec20d6404e1b2
Henry Han <hanhenry88@gmail.com>
2019-01-10 17:02:00 +0800

    Extract into fragment

--- src/index.js
+++ src/index.js
@@ -15,20 +15,42 @@ const client = new ApolloClient({
   },
 });
 
-const GET_ORGANIZATION = gql`
-  {
-    organization(login: "the-road-to-learn-react") {
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  query($organization: String!) {
+    organization(login: $organization) {
       name
       url
+      repositories(
+        first: 5
+        orderBy: { direction: ASC, field: STARGAZERS }
+      ) {
+        edges {
+          node {
+            ...repository
+          }
+        }
+      }
+    }
+  }
+  fragment repository on Repository {
+    name
+    url
+    stargazers {
+      totalCount
     }
   }
 `;
 
 client
   .query({
-    query: GET_ORGANIZATION,
+    query: GET_REPOSITORIES_OF_ORGANIZATION,
+    variables: { organization: 'the-road-to-learn-react' },
   })
-  .then(console.log);
+  .then(res => {
+    res.data.organization.repositories.edges.forEach(edge => {
+      console.log(edge.node.stargazers.totalCount);
+    });
+  });
 
 const userCredentials = { firstname: 'Robin' };
 const userDetails = { nationality: 'German' };
